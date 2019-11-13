c3b276bb4985042d1ec4ab27104dce764665c3b3
NazarTsybulskyi <NazarTsybulskyi@users.noreply.github.com>
2019-11-13 11:25:17 +0200

    fixed zoomlevel

--- force-app/main/default/aura/AccountMap/AccountMapController.js
+++ force-app/main/default/aura/AccountMap/AccountMapController.js
@@ -4,11 +4,11 @@
 		getAllAccountLocationsAction.setCallback(this, function(response){
             if (response.getState() == "SUCCESS"){
 				component.set("v.mapMarkers", response.getReturnValue()); 
-				component.set("v.zoomLevel", 1);
+				component.set("v.zoomLevel", 4);
 				//component.set("v.center", )
 				console.log('current zoom level -->>> ' + component.getZoom());
             } else {
-				console.log("Response state: " + response.getState() + ': ' + response.getMessage());
+				console.log('Error: '  + response.getMessage());
 			}
         });
         $A.enqueueAction(getAllAccountLocationsAction);
